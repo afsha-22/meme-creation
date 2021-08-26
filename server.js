49d006508e77389b07bd32e3ba938e322704749b
kcsheng <kacysheng@mail.com>
2021-08-27 03:52:43 +1000

    Add and test bootstrap

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const models = require("./models");
 
 const express = require("express");
+const app = express();
 const path = require("path");
 const db = require("./clients/db");
 const exphbs = require("express-handlebars");
-const app = express();
 const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-const models = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("home");
 });
 
 db.sync().then(() => {
