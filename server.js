faadc1197ef373bb75f3e85a382a513936112dec
kcsheng <kacysheng@mail.com>
2021-08-26 21:44:13 +1000

    Set up handlebar engine

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const db = require('./clients/db')
-const app = express()
+const express = require("express");
+const db = require("./clients/db");
+const exphbs = require("express-handlebars");
+const app = express();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const models = require('./models')
+const models = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
- 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 db.sequelize.sync().then(() => {
-    app.listen(PORT)
+  app.listen(PORT);
 });
